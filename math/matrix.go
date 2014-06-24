555471bea91cbba547c68738762b1dabc6a6372d
Kevin K <kbknapp@gmail.com>
2014-06-23 22:56:19 -0400

    adding Diagnals

--- math/matrix.go
+++ math/matrix.go
@@ -1,18 +1,20 @@
 package math
 
 type Matrix struct {
-	M    []int
-	Size int
-	Rows [][]int
-	Cols [][]int
+	M     []int
+	Size  int
+	Rows  [][]int
+	Cols  [][]int
+	Diags [][]int
 }
 
 func NewMatrix(s int) Matrix {
 	m := Matrix{
-		M:    make([]int, s*s),
-		Size: s,
-		Rows: make([][]int, s),
-		Cols: make([][]int, s),
+		M:     make([]int, s*s),
+		Size:  s,
+		Rows:  make([][]int, s),
+		Cols:  make([][]int, s),
+		Diags: make([][]int, 2),
 	}
 
 	r := 0
@@ -26,6 +28,18 @@ func NewMatrix(s int) Matrix {
 			m.Cols[i][j] = (j * s) + c
 		}
 		c++
+	}
+	m.Diags[0] = make([]int, s)
+	j := 0
+	for i := 0; i < (s * s); i += (s + 1) {
+		m.Diags[0][j] = i
+		j++
+	}
+	m.Diags[1] = make([]int, s)
+	j = 0
+	for i := (s - 1); i < ((s * s) - 1); i += (s - 1) {
+		m.Diags[1][j] = i
+		j++
 	}
 
 	return m
