66bbb635138dd062ac67235870999b9d2ada21a5
Kevin K <kbknapp@gmail.com>
2014-06-23 21:24:41 -0400

    added concurant prime seive

--- math/prime.go
+++ math/prime.go
@@ -1,13 +1,36 @@
 package math
 
-import gomath "math"
+// Taken from go Playground (play.golang.org/p/9U22NfrXeq)
+// Send the sequence 2, 3, 4, ... to channel 'ch'.
+func generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i // Send 'i' to channel 'ch'.
+	}
+}
 
-func IsPrime(n int) bool {
-	sr := gomath.Sqrt(float64(n))
-	for i := 2; i < int(sr); i++ {
-		if n%i == 0 {
-			return false
+// Copy the values from channel 'in' to channel 'out',
+// removing those divisible by 'prime'.
+func filterPrimes(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in // Receive value from 'in'.
+		if i%prime != 0 {
+			out <- i // Send 'i' to 'out'.
 		}
 	}
-	return true
+}
+
+// The prime sieve: Daisy-chain Filter processes.
+func NPrimes(n int) []int {
+	s := make([]int, n)
+	ch := make(chan int) // Create a new channel.
+	go Generate(ch)      // Launch Generate goroutine.
+	for i := 0; i < n; i++ {
+		prime := <-ch
+		s[i] = prime
+		ch1 := make(chan int)
+		go Filter(ch, ch1, prime)
+		ch = ch1
+	}
+
+	return s
 }
